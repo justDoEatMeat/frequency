d6922914db0cd0623e3a9067db610a7463dc219f
Aleksandr Burylov <a.burylov@outlook.com>
2019-10-14 21:37:49 +0500

    ver 1.0 1. Текст очищается от знаков препинания. 2. Текст приводится в нижний регистр и разделяется на слова 3. Каждое слово приводится в нормальную форму (ед. число, инфинитив итп) 4. Удаляются стоп слова 5. Результат записывается в csv
    Не очень хороший список стоп слов.
    Изменить csv на excel.

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 # coding=utf-8
 from collections import Counter
-import pickle
+import csv
+import time
+import pymorphy2
+from stop_words import get_stop_words
 
-f = open("C:\workPlace\\text.txt", 'r')
-f2 = open("C:\workPlace\\text2.txt", 'w')
+# "C:\workPlace\\text.txt"
+
+#read from file
+f = open(input(), 'r')
 words = f.read()
-puncts = ",.?!():;\n—"
+#f2 = open("C:\workPlace\\text2.txt", 'w')
+start_time = time.time()
 
+#symbols for remove
+puncts = ",.?!():;[]\n"
+#removing symbols
 for sym in puncts:
     words = words.replace(sym, '')
+#text in lower case and split text to list of words
+words = words.lower().split()
+#print(morph)
+
+#list of stop words for remove
+stop_words = get_stop_words('ru')
 
 
+#for normalize
+morph = pymorphy2.MorphAnalyzer()
+norm_list = []
+#normalize words and remove stop-words
+for word in words:
+    p = morph.parse(word)[0]
+    norm_list.append(p.normal_form)
+for word in norm_list:
+    if word in stop_words:
+        norm_list.remove(word)
 
-words = words.lower()
-words = words.split()
+#print(a)
 
-counter = Counter(words)
-pairs = [(pair[0], pair[1])for pair in counter.most_common()]
+#counter of words
+counter = Counter(norm_list)
+#2 columns: word - count
+pairs = [(pair[0], pair[1]) for pair in counter.most_common()]
 
+#write in csv
+with open("C:\workPlace\\text3.csv", "w", newline='') as f3:
+    writer = csv.writer(f3)
+    for row in pairs:
+        writer.writerow(row)
 
-for item in pairs:
-    n = ' '.join(map(str, item))
-
-    #n = '\n'.join(n)
-    print(n)
-    f2.write(n)
-    f2.write('\n')
+#
+print("--- %s seconds ---" % (time.time() - start_time))
